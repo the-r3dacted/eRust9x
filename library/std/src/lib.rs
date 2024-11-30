c7283a2c503e329df849a447255951653be1a3b9
Dennis Duda <git@seri.tools>
2024-11-30 16:37:03 +0100

    Add patched backtrace crate
    - Get rid of RtlCaptureContext import, manually implement it
    - Add support for older dbghelp versions. With this, backtraces on XP+
      are supported out of the box. It seems that the "best" version for old
      systems is the one from VS2005, found in
      "Microsoft Visual Studio 8\Common7\IDE\dbghelp.dll".
      This one is compiled with 9x support (unlike the VS2003 one!).

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -302,6 +302,7 @@
 #![feature(macro_metavar_expr_concat)]
 #![feature(min_specialization)]
 #![feature(must_not_suspend)]
+#![feature(naked_functions)]
 #![feature(needs_panic_runtime)]
 #![feature(negative_impls)]
 #![feature(never_type)]
